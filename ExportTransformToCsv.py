ca4e5167aaa52e502d2aeccb6160fd5c00dfb8cd
Ludvig Ström <ludvigstrom@gmail.com>
2020-04-19 01:04:15 +0200

    added bl_info

--- ExportTransformToCsv.py
+++ ExportTransformToCsv.py
@@ -1,6 +1,19 @@
 import bpy
 from math import degrees
 
+bl_info = {
+    "name": "Export Transform To Csv",
+    "author": "Ludvig Ström",
+    "version": (1, 1),
+    "blender": (2, 80, 0),
+    "location": "File > Export > Export Transform (.csv)",
+    "warning": "",
+    "description": "Exports the transform for each frame in the active timeline of a selected object",
+    "category": "Import-Export",
+    "wiki_url": ""
+}
+
+
 def write_some_data(context, filepath):
     print("Exporting CSV animation...")
     
@@ -33,9 +46,9 @@ from bpy.props import StringProperty, BoolProperty, EnumProperty
 from bpy.types import Operator
 
 
-class ExportSomeData(Operator, ExportHelper):
-    bl_idname = "export_test.some_data"  # important since its how bpy.ops.import_test.some_data is constructed
-    bl_label = "Export CSV Animation"
+class ExportTransformToCsv(Operator, ExportHelper):
+    bl_idname = "export.transform_to_csv"  # important since its how bpy.ops.import_test.some_data is constructed
+    bl_label = "Export Selected Animation (.csv)"
 
     # ExportHelper mixin class uses this
     filename_ext = ".csv"
@@ -52,16 +65,16 @@ class ExportSomeData(Operator, ExportHelper):
 
 # Only needed if you want to add into a dynamic menu
 def menu_func_export(self, context):
-    self.layout.operator(ExportSomeData.bl_idname, text="CSV Animation Export")
+    self.layout.operator(ExportTransformToCsv.bl_idname, text="Selected Animation (.csv)")
 
 
 def register():
-    bpy.utils.register_class(ExportSomeData)
+    bpy.utils.register_class(ExportTransformToCsv)
     bpy.types.TOPBAR_MT_file_export.append(menu_func_export)
 
 
 def unregister():
-    bpy.utils.unregister_class(ExportSomeData)
+    bpy.utils.unregister_class(ExportTransformToCsv)
     bpy.types.TOPBAR_MT_file_export.remove(menu_func_export)
 
 
@@ -69,4 +82,4 @@ if __name__ == "__main__":
     register()
 
     # test call
-    bpy.ops.export_test.some_data('INVOKE_DEFAULT')
+    #bpy.ops.export.transform_to_csv('INVOKE_DEFAULT')
